564af21a5f120922294967b38ded4dc28f4a3b2e
unknown <shufan@MU00042258.AD.MONASH.EDU>
2014-12-17 15:26:48 +1100

    now output matrix with columns = simyears

--- R/simulate_ddemand.R
+++ R/simulate_ddemand.R
@@ -8,7 +8,7 @@ function(model, hdata, simyears=1000, delta=5)
 	nhdata <- trunc(nrow(hdata)/seasondays/periods + 0.9999)
 	nsim <- trunc(simyears/nhdata + 0.9999)
 	nyears <- nsim*nhdata    # actual years to be simulated
-	hfit.sim <- hres.sim <- ores <- temp.sim <- matrix(NA,nyears,seasondays*periods)
+	hfit.sim <- hres.sim <- ores <- temp.sim <- matrix(NA,seasondays*periods,nyears)
 	for(i in 1:nsim){
 		# Simulated adjusted log demand
 		tmp <- newpredict(model,hdata,blocklength=35,delta=delta,periods=periods)
@@ -26,17 +26,17 @@ function(model, hdata, simyears=1000, delta=5)
 		# fix partial data at the end if any, copy and paste the corresponding part of the last year
 		if((nhdata*seasondays*periods-nh)!=0)
 			simtemp <- c(simtemp,simtemp[(length(simtemp)-seasondays*periods) + 1:(nhdata*seasondays*periods-nh)])
-		temp.sim[(i-1)*nhdata + 1:nhdata,] <- matrix(simtemp,nrow=nhdata,ncol=seasondays*periods,byrow=TRUE)
+		temp.sim[,(i-1)*nhdata + 1:nhdata] <- matrix(simtemp,nrow=seasondays*periods,ncol=nhdata,byrow=F)
 		##############################################################
         
-		hfit.sim[(i-1)*nhdata + 1:nhdata,] <- matrix(hfit,nrow=nhdata,ncol=seasondays*periods,byrow=TRUE)
-		hres.sim[(i-1)*nhdata + 1:nhdata,] <- matrix(hres,nrow=nhdata,ncol=seasondays*periods,byrow=TRUE)
+		hfit.sim[,(i-1)*nhdata + 1:nhdata] <- matrix(hfit,nrow=seasondays*periods,ncol=nhdata,byrow=F)
+		hres.sim[,(i-1)*nhdata + 1:nhdata] <- matrix(hres,nrow=seasondays*periods,ncol=nhdata,byrow=F)
 
 		# simple seasonal bootstrap for offset demand: season.bootstrap()
-		ores[(i-1)*nhdata + 1:nhdata,] <- matrix(season.bootstrap(hdata$doffset[20000:nh],20,n=nhdata*seasondays*periods,periods),
-			nrow=nhdata,ncol=seasondays*periods,byrow=TRUE)    
+		ores[,(i-1)*nhdata + 1:nhdata] <- matrix(season.bootstrap(hdata$doffset[20000:nh],20,n=nhdata*seasondays*periods,periods),
+			nrow=seasondays*periods,ncol=nhdata,byrow=F)
 	}
-   
-	return(structure(list(hhfit=c(t(hfit.sim[1:simyears,])),hhres=c(t(hres.sim[1:simyears,])),
-		ores=c(t(ores[1:simyears,])),a=model$a),class="simdemand"))
+
+	return(structure(list(hhfit=hfit.sim[,1:simyears],hhres=hres.sim[,1:simyears],
+		ores=ores[,1:simyears],a=model$a),class="simdemand"))
 }
